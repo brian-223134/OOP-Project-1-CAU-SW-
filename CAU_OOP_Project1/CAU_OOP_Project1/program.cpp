1c4ac7bb1483399bd436dcef777b3468e88fec4f
CJ <kimchanjoong54@gmail.com>
2024-10-04 18:16:37 +0900

    main function is good to go, let's make additional function

--- CAU_OOP_Project1/CAU_OOP_Project1/program.cpp
+++ CAU_OOP_Project1/CAU_OOP_Project1/program.cpp
@@ -12,11 +12,12 @@ class Student {
 private:
 	string name; //학생 이름
 	string student_id; //학생 ID
-	string Birth_year; //학생 탄생년도
-	string Department; //학생이 속한 학부
-	string Tel;
+	string birth_year; //학생 탄생년도
+	string department; //학생이 속한 학부
+	string tel;
 
 public:
+	//============= Constructor =============
 	//객체 생성자, Maual input의 값이 항상 올바르다는 것을 가정하고 생성자를 구성함
 	Student() {}
 
@@ -24,54 +25,65 @@ public:
 	{
 		this->name = name;
 		this->student_id = student_id;
-		this->Birth_year = Birth_year;
-		this->Department = Department;
-		this->Tel = Tel;
+		this->birth_year = Birth_year;
+		this->department = Department;
+		this->tel = Tel;
 	}
+
+	//============= Getter Setter =============
 	//파일에 insert할 때 사용할 Getter
 	string get_student_name() { return name; }
 	string get_id() { return student_id; }
-	string get_Birth_year() { return Birth_year; }
-	string get_Department() { return Department; };
-	string get_tel() { return Tel; };
+	string get_birth_year() { return birth_year; }
+	string get_department() { return department; };
+	string get_tel() { return tel; };
 	//insert 할 때 임시 class에 입력 값을 주기 위핸 Mutator이 필요.
-	void set_name(string& Name) { name = Name; }
-	void set_id(string id) { student_id = id; }
-	void set_Birth_year(string B_year) { Birth_year = B_year; }
-	void set_Department(string& department) { Department = department; }
-	void set_telephone(string& telephone) { Tel = telephone; }
+	void set_name(string& Name) { this->name = Name; }
+	void set_id(string& id) { this->student_id = id; }
+	void set_birth_year(string& B_year) { this->birth_year = B_year; }
+	void set_department(string& department) { this->department = department; }
+	void set_telephone(string& telephone) { this->tel = telephone; }
 };
 
 class Student_DB {
 private:
 	vector<Student> students;
+	vector<Student> _selected;
+	int sort_option = 1;
 
 public:
+	//============= Getter Setter =============
+	void set_sort_option(int sort_option) { this->sort_option = sort_option; };
+	int get_sort_option() { return this->sort_option; };
+	//============= member function =============
 	void add_student_data(Student& student);
 	void load_data(const Student& student)
 	{
 		students.push_back(student);
 	}
+	void sort_student_data(int sort_option);
 	void show_all_data()
 	{
 		cout << setw(15) << left << "Student Name" << " " << setw(10) << "StudentID"
-			<< " " << setw(20) << left << "Department" " " << setw(10)
+			<< " " << setw(25) << left << "Department" " " << setw(10)
 			<< "Birth year" << "  " << setw(12) << "Tel" << endl;
 		for (auto& s : students)
 		{
 			cout << setw(15) << left << s.get_student_name() << " "
-				<< setw(10) << s.get_id() << " " << setw(19) << s.get_Department()
-				<< " " << setw(10) << right << s.get_Birth_year()
+				<< setw(10) << s.get_id() << " " << setw(24) << s.get_department()
+				<< " " << setw(10) << right << s.get_birth_year()
 				<< "  " << setw(12) << left << s.get_tel() << endl;
 		}
 		cout << "\n";
 	}
+	//Seach function
+	void search_student_data(int select_option);
+	void sort_selected_data(int sort_option);
+	void show_selected_data();
 };
 
-// 프로그램의 기능에 직접적으로 영향을 주는 함수들
 void file_tracker(const string& f_name);
 Student get_input_student();
-bool compare_birth_year(Student& a, Student& b);
 
 
 // 선택 옵션을 보여주는 menu 함수들
@@ -81,17 +93,18 @@ void display_sorting_option();
 
 int main(void)
 {
-	const string f_name = "file1.txt"; //txt 파일의 이름이 a였으므로 파일명을 고정한다.
-	ifstream file ("file1.txt");
-	
+	const string f_name = "file1.txt"; //txt 파일의 이름이 file1였으므로 파일명을 고정한다.
+	ifstream file("file1.txt");
+
 	Student_DB students_db; //students를 DB 객체로 불러왔기 때문에 객체 속의 Vector가 생성
-	
+
 	streampos begin, end; //이전에 작성된 txt 파일 속 내용의 유무를 판단하기 위한 척도 변수
 	begin = file.tellg();
 	file.seekg(0, ios::end);
 	end = file.tellg();
 	file.close();
-	
+
+	//Synchronize file to students
 	if ((end - begin != 0)) //txt파일에 어떤 정보가 들어가있음
 	{
 		ifstream file("file1.txt");
@@ -107,8 +120,9 @@ int main(void)
 			string txt_tel;
 
 			getline(iss, txt_department, ',');
+			getline(iss, txt_name, ',');
 
-			if (iss >> txt_name >> txt_student_id >> txt_birth_year >> txt_tel)
+			if (iss >> txt_student_id >> txt_birth_year >> txt_tel)
 			{
 				students_db.load_data({ txt_name, txt_student_id,txt_birth_year, txt_department, txt_tel });
 				//student_db 객체 내로 txt 파일에 있는 내용을 전부 가져온다.
@@ -126,45 +140,31 @@ int main(void)
 		Student input_student;
 		int menu_choice;
 		cin >> menu_choice;
+		int search_choice;
+		int sort_option;
 
 		switch (menu_choice)
 		{
-		case 1:
+		case 1: //Insertion
 			input_student = get_input_student();
 			students_db.add_student_data(input_student);
 			break;
 
-		case 2:
+		case 2: //Search
 			display_search_option();
-			int op_choice;
-			cin >> op_choice;
-			switch (op_choice)
-			{
-				case 1:
-					break;
-				case 2:
-					break;
-				case 3:
-					break;
-				case 4:
-					break;
+			cin >> search_choice;
 
-				case 5:
-					break;
-
-				case 6:
-					students_db.show_all_data();
-					break;
-
-				default:
-					cout << "Invalid option" << endl;
-					break;
-			}
-			break;
-		case 3:
+			students_db.search_student_data(search_choice);
 			break;
 
-		case 4:
+		case 3: //Sorting Option
+			display_sorting_option();
+			cin >> sort_option;
+
+			students_db.set_sort_option(sort_option);
+			break;
+
+		case 4: //Exit
 			exit(0);
 			break;
 
@@ -195,22 +195,24 @@ Student get_input_student()
 	string temp_department;
 	string temp_tel;
 
+	cin.ignore();
 	cout << "Name ? ";
-	cin >> temp_name;
+	getline(cin, temp_name);
 	temp_student.set_name(temp_name);
 
 	cout << "Student ID (10 digits) ? ";
 	cin >> temp_id;
 	temp_student.set_id(temp_id);
 
+
 	cout << "Birth Year (4 digits) ? ";
 	cin >> temp_birth_year;
-	temp_student.set_Birth_year(temp_birth_year);
+	temp_student.set_birth_year(temp_birth_year);
 
 	cin.ignore(); //cin 이후 getline을 하기 위해선 cin으로 인한 버퍼 움직임을 초기화해야한다.
 	cout << "Department ? ";
 	getline(cin, temp_department); //space도 포함하는 input일 때에는 getline을 사용해야한다.
-	temp_student.set_Department(temp_department);
+	temp_student.set_department(temp_department);
 
 	cout << "Tel ? ";
 	cin >> temp_tel;
@@ -220,27 +222,27 @@ Student get_input_student()
 	return temp_student;
 }
 
-void Student_DB :: add_student_data(Student& temp_student) {
-	for (auto& exist : students){ //students vector 내를 for문을 돌면서 id가 같은지 보는 과정
+void Student_DB::add_student_data(Student& temp_student) {
+	for (auto& exist : students) { //students vector 내를 for문을 돌면서 id가 같은지 보는 과정
 		if (exist.get_id() == temp_student.get_id())
 		{
 			cout << "Error: already inserted\n";
-					return;
+			return;
 		}
 	}
 
 	students.push_back(temp_student); //위의 탐지 과정에서 중복이 없음이 확인되면 push_back을 한다.
-	
+
 	fstream file("file1.txt", ios::app); //txt 파일에 input하기 위해 fstream을 app 상태로 연다.
 	if (file.is_open())
 	{
-		string to_txt_data = temp_student.get_Department(); //Department의 경우 spacing이 있어 나중에 꺼낼 때 콤마로 string을 분리하기위한 작업
+		string to_txt_data = temp_student.get_department(); //Department의 경우 spacing이 있어 나중에 꺼낼 때 콤마로 string을 분리하기위한 작업
 		to_txt_data += ",";
 		to_txt_data += temp_student.get_student_name();
-		to_txt_data += " ";
+		to_txt_data += ",";
 		to_txt_data += temp_student.get_id();
 		to_txt_data += " ";
-		to_txt_data += temp_student.get_Birth_year();
+		to_txt_data += temp_student.get_birth_year();
 		to_txt_data += " ";
 		to_txt_data += temp_student.get_tel();
 		file << to_txt_data << endl;
@@ -248,9 +250,166 @@ void Student_DB :: add_student_data(Student& temp_student) {
 	file.close();
 }
 
-bool compare_birth_year(Student& a, Student& b)
+void Student_DB::sort_student_data(int sort_option) //now not be used
 {
-	return a.get_Birth_year() < b.get_Birth_year();
+	//<algorithm> sort, 람다 함수 사용
+	switch (sort_option)
+	{
+	case 1: //sort by name
+		sort(students.begin(), students.end(), [](Student& a, Student& b) {
+			return a.get_student_name() < b.get_student_name();
+			});
+		break;
+
+	case 2: //sort by student ID
+		sort(students.begin(), students.end(), [](Student& a, Student& b) {
+			return a.get_id() < b.get_id();
+			});
+		break;
+
+	case 3: //sort by birth year
+		sort(students.begin(), students.end(), [](Student& a, Student& b) {
+			return a.get_birth_year() < b.get_birth_year();
+			});
+		break;
+
+	case 4: //sort by department name
+		sort(students.begin(), students.end(), [](Student& a, Student& b) {
+			return a.get_department() < b.get_department();
+			});
+		break;
+
+	default:
+		break;
+	}
+}
+
+void Student_DB::show_selected_data() //- for Student_DB::search_student_data()
+{
+	cout << setw(15) << left << "Student Name" << " " << setw(10) << "StudentID"
+		<< " " << setw(25) << left << "Department" " " << setw(10)
+		<< "Birth year" << "  " << setw(12) << "Tel" << endl;
+	for (auto& s : _selected)
+	{
+		cout << setw(15) << left << s.get_student_name() << " "
+			<< setw(10) << s.get_id() << " " << setw(24) << s.get_department()
+			<< " " << setw(10) << right << s.get_birth_year()
+			<< "  " << setw(12) << left << s.get_tel() << endl;
+	}
+	cout << "\n";
+}
+
+void Student_DB::search_student_data(int select_option) //search and print
+{
+	//flow :: select -> _selected.push -> _selected print -> _selected.clear
+	//.compare() :: string equal -> 0
+
+	string keyword; //Search keyword
+	string addmission_year; //학번 앞 4자리
+
+	//============= select process =============
+	switch (select_option)
+	{
+	case 1: //Search by name
+		cout << "Name keyword ? "; //get key word
+		cin.ignore();
+		getline(cin, keyword); //이름은 space가 존재하므로 getline으로 받는다.
+		for (auto& s : students) //check each student and select (same with under code)
+		{
+			if (s.get_student_name().compare(keyword) == 0)
+				_selected.push_back(s);
+		}
+		break;
+
+	case 2: //Search by Student ID
+		cout << "Student ID keyword ? ";
+		cin >> keyword;
+		for (auto& s : students)
+		{
+			if (s.get_id().compare(keyword) == 0)
+				_selected.push_back(s);
+		}
+		break;
+
+	case 3: //Search by Addmission year
+		cout << "Addmission year keyword ? ";
+		cin >> keyword;
+		for (auto& s : students)
+		{
+			addmission_year = (s.get_id()).substr(0, 4); //학번 앞 4자리 추출
+			if (addmission_year.compare(keyword) == 0)
+				_selected.push_back(s);
+		}
+		break;
+
+	case 4: //Search by birth year
+		cout << "birh year keyword ? ";
+		cin >> keyword;
+		for (auto& s : students)
+		{
+			if (s.get_birth_year().compare(keyword) == 0)
+				_selected.push_back(s);
+		}
+		break;
+
+	case 5: //Search by Department name
+		cout << "Department name keyword ? ";
+		cin.ignore();
+		getline(cin, keyword); //department도 space를 허용하기에 getline으로 받는다.
+		for (auto& s : students)
+		{
+			if (s.get_department().compare(keyword) == 0)
+				_selected.push_back(s);
+		}
+		break;
+
+	case 6: //idea :: List All : select all == students
+		_selected = students;
+		break;
+
+	default:
+		cout << "Invalid option\n";
+		break;
+	}
+
+	//============= sort -> print -> clear =============
+	sort_selected_data(sort_option);
+	show_selected_data();
+	_selected.clear();
+}
+
+void Student_DB::sort_selected_data(int sort_option) //기존 sort_students_data()에서 students->_selected로만 변경
+{
+	//<algorithm> sort, 람다 함수 사용
+	switch (sort_option)
+	{
+	case 1: //sort by name
+		sort(_selected.begin(), _selected.end(), [](Student& a, Student& b) {
+			return a.get_student_name() < b.get_student_name();
+			});
+		break;
+
+	case 2: //sort by student ID
+		sort(_selected.begin(), _selected.end(), [](Student& a, Student& b) {
+			return a.get_id() < b.get_id();
+			});
+		break;
+
+	case 3: //sort by birth year
+		sort(_selected.begin(), _selected.end(), [](Student& a, Student& b) {
+			return a.get_birth_year() < b.get_birth_year();
+			});
+		break;
+
+	case 4: //sort by department name
+		sort(_selected.begin(), _selected.end(), [](Student& a, Student& b) {
+			return a.get_department() < b.get_department();
+			});
+		break;
+
+	default:
+		break;
+	}
 }
 
 void display_menu()
@@ -259,7 +418,7 @@ void display_menu()
 	cout << "2. Search\n";
 	cout << "3. Sorting Option\n";
 	cout << "4. Exit\n";
-	cout << "_> ";
+	cout << "> ";
 }
 
 void display_search_option()
@@ -275,7 +434,7 @@ void display_search_option()
 
 void display_sorting_option()
 {
-	cout << "-Sorting Option\n";
+	cout << "- Sorting Option\n";
 	cout << "1. Sort by Name\n";
 	cout << "2. Sort by Student ID\n";
 	cout << "3. Sort by Birth Year\n";
